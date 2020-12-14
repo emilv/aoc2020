d20daab45972e5dd21aeb7caba219aef3e101694
Emil Vikström <emil.vikstrom@kvdbil.se>
2020-12-14 21:47:11 +0100

    December 5, first puzzle. Parse boarding passses

---
+++ 5/main.py
@@ -0,0 +1,31 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class BoardingPass:
+    chars: str
+    row: int
+    col: int
+    seat_id: int
+
+
+def parse(line: str) -> BoardingPass:
+    def num(chars: str, lower: int, upper: int) -> int:
+        if not chars:
+            return upper
+        middle = (upper - lower) // 2 + lower
+        if chars[0] in ("F", "L"):
+            return num(chars[1:], lower, middle)
+        else:
+            return num(chars[1:], middle + 1, upper)
+
+    row = num(line[:7], 0, 127)
+    col = num(line[7:], 0, 7)
+    seat_id = row * 8 + col
+    return BoardingPass(chars=line, row=row, col=col, seat_id=seat_id)
+
+
+with open("input", "r") as f:
+    boarding_passes = [parse(line.strip()) for line in f]
+
+print(max([bp.seat_id for bp in boarding_passes]))
